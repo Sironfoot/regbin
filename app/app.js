b2ed2825a72ad514fcb7c091ab7c8340ac9a8b49
Dominic Pettifer <sironfoot@gmail.com>
2013-09-17 23:15:22 +0100

    Start-up code to ensure common regex expression patterns are in the database

--- app/app.js
+++ app/app.js
@@ -8,6 +8,8 @@ var http = require('http');
 var path = require('path');
 var cons = require('consolidate');
 var gu = require('guthrie');
+var async = require('async');
+var fs = require('fs');
 
 var app = express();
 app.engine('html', cons.dust);
@@ -29,10 +31,52 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-var router = new gu.Router(app, __dirname);
 
+// Set-up router
+var router = new gu.Router(app, __dirname);
 router.mapRoute('/', { controller: 'home', action: 'index' });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('RegBin listening on port ' + app.get('port'));
+
+// Ensure Common Expressions are present
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/regbin', function(err, db) {
+    if (err) throw err;
+    
+    var collection = db.collection('common');
+    
+    var commonDataPath = path.join(__dirname, 'lib', 'commonRegex.json');
+    fs.readFile(commonDataPath, { encoding: 'utf-8' }, function(err, data) {
+        if (err) throw err;
+        
+        var commonExpressions = JSON.parse(data.toString());
+        
+        async.each(commonExpressions,
+            function saveToDatabase(expression, done) {
+                
+                collection.findOne({ name: expression.name }, function(err, document) {
+                    if (err) throw err;
+                    
+                    if (document) {
+                        done();
+                    }
+                    else {
+                        collection.insert(expression, function(err, docs) {
+                            if (err) throw err;
+                            done();
+                        });
+                    }
+                });
+            },
+            function finished(err) {
+                // Finished checking, run app
+                http.createServer(app).listen(app.get('port'), function(){
+                    console.log('RegBin listening on port ' + app.get('port'));
+                });
+            });
+    });
 });
+
+
+
+
